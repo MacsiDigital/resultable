a35dea3c5baf6282d350232d877a3eb211304633
Colin Hall <colin@macsi.co.uk>
2019-03-25 23:34:53 +0000

    Apply fixes from StyleCI

--- config/config.php
+++ config/config.php
@@ -4,12 +4,12 @@
  * You can place your custom package configuration in here.
  */
 return [
-	'no_results' => [
-		'20',
-		'50',
-		'100',
-		'250',
-		'500',
-		'1000'
-	]
+    'no_results' => [
+        '20',
+        '50',
+        '100',
+        '250',
+        '500',
+        '1000',
+    ],
 ];
